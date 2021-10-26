981c0e51dec356cdf5dab8bea2fe6846cf451e17
dimachyrkov <88532865+deman4ela@users.noreply.github.com>
2021-10-26 13:39:40 +0300

    Moving 'show loader' logic in fetchHeroes action

--- src/redux/sagas/heroesSaga.tsx
+++ src/redux/sagas/heroesSaga.tsx
@@ -1,6 +1,6 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
 import { FETCH_HEROES }  from '../types';
-import { fetchHeroes, fetchHeroesSuccess, fetchHeroesError, showLoaderHeroes, hideLoaderHeroes } from '../actions';
+import { fetchHeroes, fetchHeroesSuccess, fetchHeroesError, hideLoaderHeroes } from '../actions';
 import { getHeroes, getAPIResource, getAllHeroes } from '../../api/api';
 
 export default function* heroesSagaWatcher() {
@@ -9,7 +9,6 @@ export default function* heroesSagaWatcher() {
 
 function* heroesSagaWorker(action: any): any {
   try {
-    yield put(showLoaderHeroes());
     const payload = yield call(getHeroes, action.payload);
     yield put(fetchHeroesSuccess(payload));
     yield put(hideLoaderHeroes());
