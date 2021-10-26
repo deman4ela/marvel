6f0a42b03e7d1f8236865db14557ede415fc8edf
dimachyrkov <88532865+deman4ela@users.noreply.github.com>
2021-10-26 13:38:17 +0300

    Moving 'show loader' logic in fetchHeroes action

--- src/redux/reducers/comicsReducer.tsx
+++ src/redux/reducers/comicsReducer.tsx
@@ -1,4 +1,4 @@
-import { FETCH_HEROES, FETCH_HEROES_SUCCESS, FETCH_HEROES_ERROR, FETCH_COMICS_SUCCESS, FETCH_COMICS_ERROR, SHOW_LOADER_COMICS, HIDE_LOADER_COMICS }  from '../types';
+import { FETCH_HEROES, FETCH_HEROES_SUCCESS, FETCH_HEROES_ERROR, FETCH_COMICS, FETCH_COMICS_SUCCESS, FETCH_COMICS_ERROR, HIDE_LOADER_COMICS }  from '../types';
 
 const defaultComicsState = {
   fetchedComicsSuccess: [],
@@ -9,14 +9,14 @@ const defaultComicsState = {
 
 const comicsReducer = (state = defaultComicsState, action: any) => {
   switch (action.type) {
+    case FETCH_COMICS:
+      return { ...state, loaderForComics: true };
+
     case FETCH_COMICS_SUCCESS:
       return { ...state, fetchedComicsSuccess: action.payload };
 
     case FETCH_COMICS_ERROR:
       return { ...state, fetchedComicsError: action.payload };
-
-    case SHOW_LOADER_COMICS:
-      return { ...state, loaderForComics: true };
 
     case HIDE_LOADER_COMICS:
       return { ...state, loaderForComics: false };
