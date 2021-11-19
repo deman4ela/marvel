5d43de110cbfde89e6db00fd2263fe1000ac193f
dimachyrkov <88532865+deman4ela@users.noreply.github.com>
2021-11-19 15:01:35 +0200

    Add getAPIResourceTEST api function for sorting

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -30,3 +30,22 @@ export function getAllHeroes() {
 export function getAllComicsOfHero(heroID: any) {
   return getAPIResource(`characters/${heroID}/comics`);
 }
+
+export async function getAPIResourceTEST(path: string, searchedHeroName?: string, orderBy?: string): Promise<AxiosResponse> {
+  const res = await axios.get(`${APIBaseURL}${path}`, {
+    params: {
+      apikey: REACT_APP_API_KEY,
+      hash: REACT_APP_API_HASH,
+      ts: 1,
+      limit: 5,
+      nameStartsWith: searchedHeroName,
+      orderBy
+    },
+  });
+  try {
+    const { results } = res.data.data;
+    return results;
+  } catch (error) {
+    throw new Error('Error');
+  }
+};
