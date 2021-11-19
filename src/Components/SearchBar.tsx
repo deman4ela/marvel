2994263b242c50e611e3327d5be0e086d9a3bfbb
dimachyrkov <88532865+deman4ela@users.noreply.github.com>
2021-11-19 15:03:05 +0200

    Update SearchBar.tsx

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -3,14 +3,20 @@ import { JsxElement } from 'typescript';
 import '../index.css';
 
 function SearchBar(props: any) {
-  const { handleSubmit, handleChange } = props;
+  const { handleSubmit, handleChange, handleClickAsc, handleClickDesc } = props;
 
   return (
-    <div className='searchBar'>
-      <form onSubmit={handleSubmit}>
-        <input type='text' name='query' placeholder='Type a hero' onChange={handleChange} className='searchBar__input' />
-        <button type='submit'className='searchBar__button'> Search </button>
-      </form>
+    <div>
+      <div className='searchBar'>
+        <form onSubmit={handleSubmit}>
+          <input type='text' name='query' placeholder='Type a hero' onChange={handleChange} className='searchBar__input' />
+          <button type='submit'className='searchBar__button'> Search </button>
+        </form>
+      </div >
+      <div className='sorting'>
+        <button type='submit'className='searchBar__button' onClick={handleClickAsc}> Sort A-Z </button>
+        <button type='submit'className='searchBar__button' onClick={handleClickDesc}> Sort Z-A </button>
+      </div>
     </div>
   );
 }
